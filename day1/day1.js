510910963042785f0b197b3e9615f8dfb6adfce9
benji77fr <b.girardpro@gmail.com>
2021-12-01 23:31:54 +0100

    Change algo and it works YEAH !

--- day1/day1.js
+++ day1/day1.js
@@ -2,14 +2,23 @@ const fs = require('fs');
 const input = fs.readFileSync('./input.txt', 'utf-8')
 const inputArr = input.split('\n')
 
-let increased = 0
-let previousValue = 0
+const count = () => {
+  let increased = 0
+  let previousValue = 0
+  let nextOne = 0
 
-inputArr.forEach((value, index) => {
-  index === 0 ? previousValue = value : null
+  for (let i = 0; i < inputArr.length - 1; i++) {
+    previousValue = Number(inputArr[i]);
+    nextOne = Number(inputArr[i + 1]);
+  
+    console.log(previousValue, nextOne);
+  
+    if (nextOne > previousValue) {
+      increased = increased + 1;
+    }
+  }
 
-  value > previousValue ? increased++ : null
-  previousValue = value
-})
+  return increased
+}
 
-console.log(increased);
+console.log(count());
